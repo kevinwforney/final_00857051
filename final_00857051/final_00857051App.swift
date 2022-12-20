6038376ad129ff186cdc79b7803f6b3418648687
fuck <fuck@ass.hole>
2022-12-21 00:27:21 +0800

    Initial Commit

---
+++ final_00857051/final_00857051App.swift
@@ -0,0 +1,17 @@
+//
+//  final_00857051App.swift
+//  final_00857051
+//
+//  Created by User03 on 2022/12/21.
+//
+
+import SwiftUI
+
+@main
+struct final_00857051App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
